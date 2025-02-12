8af72743bd37ec403ec36144727e212a32d500e9
apamanoko <apamanoko221@yahoo.co.jp>
2025-02-12 18:00:15 -0500

    Drag n Drop Layout

--- components/custom/Canvas.jsx
+++ components/custom/Canvas.jsx
@@ -1,13 +1,46 @@
 'use client'
-import { useScreenSizeContext } from '@/app/provider'
-import React from 'react'
+import { useScreenSizeContext, useDragElementLayoutContext, useEmailTemplateContext } from '@/app/provider'
+import React, { useState } from 'react'
+import ColumnLayout from '../LayoutElements/ColumnLayout';
 
 function Canvas() {
   const { screenSize, serScreenSize } = useScreenSizeContext();
+  const {dragElementLayout, setDragElementLayout} = useDragElementLayoutContext();
+  const {emailTemplate, setEmailTemplate} = useEmailTemplateContext();
+  const [dragOver, setDragOver] = useState(false);
+
+  const onDragOver=(e)=>{
+    e.preventDefault();
+    setDragOver(true);
+  }
+  const onDrop=()=>{
+    setDragOver(false);
+    if(dragElementLayout?.dragLayout){
+      setEmailTemplate(prev=>[...prev, dragElementLayout?.dragLayout]);
+    }
+  } 
+
+  const getLayoutComponent=(layout)=>{
+    if(layout?.type==='column'){
+      return <ColumnLayout layout={layout} />
+    }
+  }
+
   return (
-    <div　className='mt-20 flex justify-center'>
+    <div className='mt-20 flex justify-center'>
       <div className={`bg-white p-6 w-full 
-        ${screenSize=='desktop'?'max-w-2xl':'max-w-md'}`}>
+        ${screenSize=='desktop'?'max-w-2xl':'max-w-md'}
+        ${dragOver && 'bg-green-100 p-4'}
+      `}
+        onDragOver={onDragOver}
+        onDrop={onDrop} 
+      >
+        {emailTemplate.length>0 ? emailTemplate.map((layout, index)=>(
+          <div key={index}>
+            {getLayoutComponent(layout)}
+          </div>
+        )): <h2 className='p-4 text-center bg-gray-100 border border-dashed'>Add Layout Here</h2>
+        }
 
       </div>
     </div>
